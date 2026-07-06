a6f964afff53eafc9f2826e0eac33583466d0cd8
Syed Muzammil Husain <muzammil@gece.edu.pk>
2026-07-06 17:33:53 +0500

    Security: block crawlers (robots.txt + .htaccess), disable right-click/copy/devtools/source-view, anti-iframe, no source maps, anti-hotlinking

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Disable source maps in production (prevents source code exposure)
+    sourcemap: false,
+    // Minify aggressively
+    minify: 'esbuild',
+    // Remove console.log in production
+    esbuild: {
+      drop: ['debugger'],
+    },
+  },
 })
